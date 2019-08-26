803885ed491ebd66435fe1a4a43f421885996fc1
priyan-coder <rajamohanpriyan@gmail.com>
2019-08-26 14:40:22 +0800

    Done with Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -11,15 +12,32 @@ public class Duke {
         System.out.println(logo + "\n"+ "Hello! I'm Duke\n" +
                 "     What can I do for you?\n");
 
+
+        ArrayList<String> ar = new ArrayList<String>();
         while (true) {
+            //String[] myStringArray = new String[100];
             Scanner sc = new Scanner(System.in);
             String line = sc.nextLine();
 
             if(line.toLowerCase().equals("bye")){
                 System.out.println(" Bye. Hope to see you again soon!\n");
+                ar.clear();
                 break;
+            }else if (line.toLowerCase().equals("list")){
+
+                System.out.println( "____________________________________________________________ \n");
+
+                for(int i = 0; i < ar.size(); ++i){
+                    System.out.println((i+1) + ". " + ar.get(i));
+                }
+
+                System.out.println( "____________________________________________________________ \n");
             }else{
-                System.out.println(line);
+                ar.add(line);
+                System.out.println(" ____________________________________________________________\n" +
+                        "     added: " + line + " \n" +
+                        " ____________________________________________________________\n" +
+                        "\n");
             }
 
         }
