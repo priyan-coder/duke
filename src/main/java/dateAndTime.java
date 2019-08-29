61b42a5965c9b2680c2676ad4a438840010dca27
priyan-coder <rajamohanpriyan@gmail.com>
2019-08-29 23:23:28 +0800

    Level-8

--- src/main/java/dateAndTime.java
+++ src/main/java/dateAndTime.java
@@ -20,4 +20,41 @@ public class dateAndTime {
         }
         return dateAndTime;
     }
+
+//    public String convert12(String str)
+//    {
+//        // Get Hours
+//        int h1 = (int)str.charAt(0) - '0';
+//        int h2 = (int)str.charAt(1)- '0';
+//
+//        int hh = h1 * 10 + h2;
+//
+//        // Finding out the Meridien of time
+//        // ie. AM or PM
+//        String Meridien;
+//
+//
+//        hh %= 12;
+//
+//        // Handle 00 and 12 case separately
+//        if (hh == 0) {
+//            System.out.print("12");
+//
+//            // Printing minutes and seconds
+//            for (int i = 2; i < 8; ++i) {
+//                System.out.print(str.charAt(i));
+//            }
+//        }
+//        else {
+//            System.out.print(hh);
+//            // Printing minutes and seconds
+//            for (int i = 2; i < 8; ++i) {
+//                System.out.print(str.charAt(i));
+//            }
+//        }
+//
+//        // After time is printed
+//        // cout Meridien
+//        System.out.println(" "+Meridien);
+//    }
 }
