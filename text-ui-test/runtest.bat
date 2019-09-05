cb5e7ba781da980e318ab77635fa4544108acfe0
priyan-coder <rajamohanpriyan@gmail.com>
2019-09-05 20:13:57 +0800

    Half -cooked Testing

---
+++ text-ui-test/runtest.bat
@@ -0,0 +1,33 @@
+@ECHO OFF
+
+REM create bin directory if it doesn't exist
+if not exist ..\bin mkdir ..\bin
+
+REM delete output from previous run
+del ACTUAL.txt
+del javataskData.txt
+
+REM navigate to java folder to compile
+cd ..\src\main\java
+javac *.java
+
+REM delete all class files in bin
+del ..\..\..\bin\*.class
+
+REM move all class files to bin
+move *.class ..\..\..\bin
+
+REM navigate to the bin folder now
+cd ..\..\..\bin
+
+REM move the data folder to the bin folder
+move ..\src\main\java\javataskData.txt .
+
+java Duke < ..\text-ui-test\input.txt > ..\text-ui-test\ACTUAL.txt
+FC ..\text-ui-test\ACTUAL.txt ..\text-ui-test\EXPECTED.txt
+
+REM move the data file back out
+move javataskData.txt ..\src\main\java
+
+REM return back to testing directory
+cd ..\text-ui-test
